3efca86f0ed1e6030c5ad9ca0f9bbe215a802601
Marcelo Sacrini <marcelo92_sacrini@hotmail.com>
2024-10-18 12:36:34 -0300

    Criação de variáveis e lógica da soma e cálculo da média

---
+++ teste.py
@@ -0,0 +1,13 @@
+# Definindo as duas variáveis
+num1 = 10
+num2 = 20
+
+# Somando as variáveis
+soma = num1 + num2
+
+# Calculando a média
+media = soma / 2
+
+# Exibindo os resultados
+print(f"A soma é: {soma}")
+print(f"A média é: {media}")
